23d227747f1e40cf6bcc62c33ec19d6319e722aa
parkyes90 <parkyes90@gmail.com>
2021-03-04 22:59:15 +0900

    feature: Styling header nav

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,6 +2,11 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :user
 
+  validates :user, :course, presence: true
+
+  validates_presence_of :rating, if: :review?
+  validates_presence_of :review, if: :rating?
+
   validates_uniqueness_of :user_id, scope: :course_id  #user cant be subscribed to the same course twice
   validates_uniqueness_of :course_id, scope: :user_id  #user cant be subscribed to the same course twice
 
@@ -26,4 +31,5 @@ class Enrollment < ApplicationRecord
       end
     end
   end
+
 end
