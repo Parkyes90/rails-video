28d7327179bb7b1be6f90097afc7b1c4c889d62c
parkyes90 <parkyes90@gmail.com>
2021-03-05 23:13:08 +0900

    feat: Add ransack to courses

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,8 +5,17 @@ class EnrollmentsController < ApplicationController
   # GET /enrollments
   # GET /enrollments.json
   def index
+    @ransack_path = enrollments_path
+
     @q = Enrollment.ransack(params[:q])
     @pagy, @enrollments = pagy(@q.result.includes(:user))
+  end
+
+  def my_students
+    @ransack_path = my_students_enrollments_path
+    @q = Enrollment.joins(:course).where(courses: {user: current_user}).ransack(params[:q])
+    @pagy, @enrollments = pagy(@q.result.includes(:user))
+    render 'index'
   end
 
   # GET /enrollments/1
