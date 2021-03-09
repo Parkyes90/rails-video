6020d224131510dfbc17029df52507ce9e6005b4
parkyes90 <parkyes90@gmail.com>
2021-03-09 22:46:09 +0900

    feat: validation for destroy

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -82,12 +82,13 @@ class CoursesController < ApplicationController
 
   def destroy
     authorize @course
-    @course.destroy
-    respond_to do |format|
-      format.html do
-        redirect_to courses_url, notice: 'Course was successfully destroyed.'
+    if @course.destroy
+      respond_to do |format|
+        format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+        format.json { head :no_content }
       end
-      format.json { head :no_content }
+    else
+      redirect_to @course, alert: 'Course has enrollments. Can not be destroyed.'
     end
   end
 
