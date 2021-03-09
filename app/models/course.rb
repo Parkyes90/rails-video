3e26a190e6148bd6158dfcf42c30f969960a9dca
parkyes90 <parkyes90@gmail.com>
2021-03-09 22:17:41 +0900

    feat: Add Track user Progress in a course

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,6 +10,8 @@ class Course < ApplicationRecord
   belongs_to :user, counter_cache: true
   has_many :lessons, dependent: :destroy
   has_many :enrollments
+  has_many :user_lessons, through: :lessons
+
   validates :title, uniqueness: true
 
   scope :latest, -> { limit(3).order(created_at: :desc) }
@@ -37,6 +39,12 @@ class Course < ApplicationRecord
 
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
+  end
+
+  def progress(user)
+    unless self.lessons_count == 0
+      user_lessons.where(user: user).count/self.lessons_count.to_f*100
+    end
   end
 
   def update_rating
