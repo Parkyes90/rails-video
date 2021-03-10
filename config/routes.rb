30d11c0a745e7f407f3e069625eb0fcac0b1b50f
parkyes90 <parkyes90@gmail.com>
2021-03-10 23:23:52 +0900

    feature: Add charts

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'activity', to: 'home#activity'
+  get 'analytics', to: 'home#analytics'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
