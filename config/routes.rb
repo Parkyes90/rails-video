a2813c7431ce53d6bfb50f6e860ca91467d3b3a8
parkyes90 <parkyes90@gmail.com>
2021-03-11 23:18:59 +0900

    feature: Add namespace charts in routes, group by sum

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,11 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
-  get 'charts/users_per_day', to: 'charts#users_per_day'
-  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
-  get 'charts/course_popularity', to: 'charts#course_popularity'
+  namespace :charts do
+    get 'users_per_day'
+    get 'enrollments_per_day'
+    get 'course_popularity'
+    get 'money_makers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
