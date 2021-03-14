eeda65a8b70ddf5fac8265443fe437dbaa35609e
parkyes90 <parkyes90@gmail.com>
2021-03-14 20:39:09 +0900

    feature: add course policy

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,18 +1,35 @@
 class Lesson < ApplicationRecord
   belongs_to :course, counter_cache: true
   has_many :user_lessons, dependent: :destroy
+
   validates :title, :content, :course, presence: true
+
   has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  include RankedModel
+  ranks :row_order, with_same: :course_id
 
   def to_s
     title
   end
 
+  def prev
+    course.lessons.where("row_order < ?", row_order).order(:row_order).last
+  end
+
+  def next
+    course.lessons.where("row_order > ?", row_order).order(:row_order).first
+  end
+
   def viewed(user)
     self.user_lessons.where(user: user).present?
+    #self.user_lessons.where(user_id: [user.id], lesson_id: [self.id]).present?
   end
+
 end
