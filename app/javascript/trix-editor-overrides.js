01bf480de58b7f3b78edc9012053e1ca10da7a63
parkyes90 <parkyes90@gmail.com>
2021-03-19 23:05:58 +0900

    feature: Add allow file upload, video preview to blob

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -1,5 +1,7 @@
-// Blacklist all file attachments
 window.addEventListener("trix-file-accept", function (event) {
-  event.preventDefault();
-  alert("File attachment not supported!");
+  const maxFileSize = 3000 * 3000; // around 9MB
+  if (event.file.size > maxFileSize) {
+    event.preventDefault();
+    alert("Only support attachment files upto size 9MB!");
+  }
 });
