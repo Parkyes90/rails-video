6e4600b7f3162aa79f6bb4a182cbe81c57aee421
parkyes90 <parkyes90@gmail.com>
2021-03-20 22:37:47 +0900

    feature: add public_acivity comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ApplicationRecord
-
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
   belongs_to :user
   belongs_to :lesson
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
   validates :content, presence: true
-
+  def to_s
+    content
+  end
 end
