17f019a64a5c40fad27d11380155671593cf092d
parkyes90 <parkyes90@gmail.com>
2021-03-24 21:54:39 +0900

    add search by tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,11 @@ class Tag < ApplicationRecord
   has_many :courses, through: :course_tags
 
   validates :name, length: {minimum: 1, maximum: 25}, uniqueness: true
+  def to_s
+    name
+  end
 
+  def popular_name
+    "#{name.to_s}: #{course_tags_count.to_s}"
+  end
 end
