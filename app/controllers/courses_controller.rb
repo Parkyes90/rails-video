6cf89482d9ff0b3f6f9dfee32ac2d79ba72616f0
parkyes90 <parkyes90@gmail.com>
2021-03-24 22:06:03 +0900

    add admin manage tag

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,7 +6,7 @@ class CoursesController < ApplicationController
     @ransack_path = courses_path
     @ransack_courses = Course.published.approved.ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
-    @tags = Tag.all
+    @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
 
   end
 
@@ -14,14 +14,14 @@ class CoursesController < ApplicationController
     @ransack_path = learning_courses_path
     @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
-    @tags = Tag.all
+    @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
   end
 
   def pending_review
     @ransack_path = pending_review_courses_path
     @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
-    @tags = Tag.all
+    @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
     render 'index'
   end
 
@@ -29,7 +29,7 @@ class CoursesController < ApplicationController
     @ransack_path = teaching_courses_path
     @ransack_courses = Course.where(user: current_user).ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
-    @tags = Tag.all
+    @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
     render 'index'
   end
 
@@ -37,7 +37,7 @@ class CoursesController < ApplicationController
     @ransack_path = unapproved_courses_path
     @ransack_courses = Course.unapproved.ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
-    @tags = Tag.all
+    @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
     render 'index'
   end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,12 @@ class HomeController < ApplicationController
     @latest = Course.latest.published.approved
     @top_rated = Course.top_rated.published.approved
     @popular = Course.popular.published.approved
-    @purchased_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(3)
+    if current_user
+      @learning_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(3)
+      @teaching_courses = current_user.courses
+    end
+    @popular_tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc).limit(10)
+
   end
 
   def activity
@@ -21,6 +26,7 @@ class HomeController < ApplicationController
       redirect_to root_path, alert: "You are not authorized to access this page"
     end
   end
+
   def privacy_policy
   end
 end
