5ba9d3b2f84ff94e1bc87b658cece2a7a30ec62f
parkyes90 <parkyes90@gmail.com>
2021-03-26 23:24:46 +0900

    add layout for wizard step, remove course edit action

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,11 +3,28 @@ class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy, :approve, :unapprove, :analytics]
 
   def index
+    #if params[:title]
+    #  @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
+    #else
+    #  #@courses = Course.all
+    #
+    #  #@q = Course.ransack(params[:q])
+    #  #@courses = @q.result.includes(:user)
+    #end
+
+    #if current_user.has_role?(:admin)
+    #  @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    #  @courses = @ransack_courses.result.includes(:user)
+    #else
+    #  redirect_to root_path, alert: 'You do not have access'
+    #end
+
     @ransack_path = courses_path
+
     @ransack_courses = Course.published.approved.ransack(params[:courses_search], search_key: :courses_search)
+    #@courses = @ransack_courses.result.includes(:user)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
     @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
-
   end
 
   def learning
@@ -15,6 +32,7 @@ class CoursesController < ApplicationController
     @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user, :course_tags, :course_tags => :tag))
     @tags = Tag.all.where.not(course_tags_count: 0).order(course_tags_count: :desc)
+    render 'index'
   end
 
   def pending_review
@@ -69,11 +87,6 @@ class CoursesController < ApplicationController
     @tags = Tag.all
   end
 
-  def edit
-    authorize @course
-    @tags = Tag.all
-  end
-
   def create
     @course = Course.new(course_params)
     authorize @course
@@ -86,20 +99,6 @@ class CoursesController < ApplicationController
       else
         @tags = Tag.all
         format.html { render :new }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    authorize @course
-    respond_to do |format|
-      if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-        format.json { render :show, status: :ok, location: @course }
-      else
-        @tags = Tag.all
-        format.html { render :edit }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
