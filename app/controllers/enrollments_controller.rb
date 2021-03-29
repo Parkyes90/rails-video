273af1943ce7c24869d31fcf846b3fa3ce50ebc0
parkyes90 <parkyes90@gmail.com>
2021-03-29 22:45:51 +0900

    chore: add payments business logic

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,75 +3,24 @@ class EnrollmentsController < ApplicationController
   before_action :set_enrollment, only: [:show, :edit, :update, :destroy, :certificate]
   before_action :set_course, only: [:new, :create]
 
-  # GET /enrollments
-  # GET /enrollments.json
   def index
+    #@enrollments = Enrollment.all
+    #@pagy, @enrollments = pagy(Enrollment.all)
+
+
     @ransack_path = enrollments_path
 
     @q = Enrollment.ransack(params[:q])
     @pagy, @enrollments = pagy(@q.result.includes(:user))
+
+    authorize @enrollments
   end
 
-  def my_students
-    @ransack_path = my_students_enrollments_path
+  def teaching
+    @ransack_path = teaching_enrollments_path
     @q = Enrollment.joins(:course).where(courses: {user: current_user}).ransack(params[:q])
     @pagy, @enrollments = pagy(@q.result.includes(:user))
     render 'index'
-  end
-
-  # GET /enrollments/1
-  # GET /enrollments/1.json
-  def show
-  end
-
-  # GET /enrollments/new
-  def new
-    @enrollment = Enrollment.new
-  end
-
-  # GET /enrollments/1/edit
-  def edit
-    authorize @enrollment
-  end
-
-  # POST /enrollments
-  # POST /enrollments.json
-  def create
-    if @course.price > 0
-      flash[:alert] = "You can not access paid courses yet."
-      redirect_to new_course_enrollment_path(@course)
-      EnrollmentMailer.student_enrollment(@enrollment).deliver_later
-      EnrollmentMailer.teacher_enrollment(@enrollment).deliver_later
-    else
-      @enrollment = current_user.buy_course(@course)
-      redirect_to course_path(@course), notice: "You are enrolled!"
-    end
-  end
-
-  # PATCH/PUT /enrollments/1
-  # PATCH/PUT /enrollments/1.json
-  def update
-    authorize @enrollment
-    respond_to do |format|
-      if @enrollment.update(enrollment_params)
-        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @enrollment }
-      else
-        format.html { render :edit }
-        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /enrollments/1
-  # DELETE /enrollments/1.json
-  def destroy
-    authorize @enrollment
-    @enrollment.destroy
-    respond_to do |format|
-      format.html { redirect_to enrollments_url, notice: 'Enrollment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   def certificate
@@ -85,16 +34,61 @@ class EnrollmentsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def new
+    @enrollment = Enrollment.new
+  end
+
+  def edit
+    authorize @enrollment
+  end
+
+  def create
+    @enrollment = current_user.buy_course(@course)
+    redirect_to course_path(@course), notice: "You are enrolled!"
+    #if @course.price > 0
+    #  flash[:alert] = "You can not access paid courses yet."
+    #  redirect_to new_course_enrollment_path(@course)
+    #else
+    #  @enrollment = current_user.buy_course(@course)
+    #  redirect_to course_path(@course), notice: "You are enrolled!"
+    #end
+  end
+
+  def update
+    authorize @enrollment
+    respond_to do |format|
+      if @enrollment.update(enrollment_params)
+        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @enrollment }
+      else
+        format.html { render :edit }
+        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    authorize @enrollment
+    @enrollment.destroy
+    respond_to do |format|
+      format.html { redirect_to enrollments_url, notice: 'Enrollment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
-    def set_course
-      @course = Course.friendly.find(params[:course_id])
-    end
+  def set_course
+    @course = Course.friendly.find(params[:course_id])
+  end
 
-    def set_enrollment
-      @enrollment = Enrollment.friendly.find(params[:id])
-    end
+  def set_enrollment
+    @enrollment = Enrollment.friendly.find(params[:id])
+  end
 
-    def enrollment_params
-      params.require(:enrollment).permit(:rating, :review)
-    end
+  def enrollment_params
+    params.require(:enrollment).permit(:rating, :review)
+  end
 end
